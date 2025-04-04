0c15358869078650578750ca95449ef3dd7c5dea
Sergey Okorochkov <okorochkov99.99@mail.ru>
2025-04-05 01:16:33 +0300

    Добавлено логирование в проект

--- json_crud.py
+++ json_crud.py
@@ -1,36 +1,58 @@
-
+import logging
 import json
+
+# Настраиваем логирование
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='log_json_py.log',
+    filemode='w',
+    encoding='utf-8',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 class Crudjson(object):
     file = None
+    logging.info(f'Класс успешно создан')
 
     def __init__(self, file):
         self.file = file
 
     def create(self, data):
+        try:
             with open (self.file, 'w', encoding='utf-8') as file:
                 json.dump(data, file, ensure_ascii=False, indent=4)
-            print(f'✅ Данные записаны в {self.file} успешно!')
+            logging.info(f'✅ Данные записаны в {self.file} успешно!')
+        except Exception as e: #почему и зачем "as e"????????????????????
+            logging.exception(f'Не удалось открыть/создать файл {self.file}')
 
 
     def add(self, new_data):
-        with open(self.file, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            print(f'Данные из файла: {data}')
-            data.update(new_data)
-            print(f'Обновленные данные из файла: {data}')
-        with open(self.file, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-            print(f'✅ Новые данные записаны в {self.file} успешно!')
+        try:
+            with open(self.file, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                logging.info(f'Данные из файла {self.file}: {data}')
+                data.update(new_data)
+                logging.info(f'Успешно добавлены данные в файл: {self.file}')
+            with open(self.file, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+                logging.info(f'✅ Новые данные записаны в {self.file} успешно!')
+        except Exception as e:
+            logging.exception(f'Не удалось добавить новые данные в файл {self.file}')
 
     def delete(self, key):
-        with open(self.file, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            del data[key]
-            print('Ключ удален!')
-        with open(self.file, 'w', encoding='utf-8') as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-            print('✅ Файл готов!' )
+        try:
+            with open(self.file, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                if key in data:
+                    del data[key]
+                    logging.info(f'Ключ {key} удален!')
+                else:
+                    logging.warning(f'Ключ {key} не найден')
+            with open(self.file, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+                logging.info(f'✅ Файл {self.file} готов, данные загружены!')
+        except Exception as e:
+            logging.exception(f'Ошибка при удалении ключа {key}')
 
 if __name__ == '__main__':
 
@@ -56,6 +78,48 @@ if __name__ == '__main__':
     writer.delete('hobby')
 
     print('ЕЛЕ СДЕЛАЛ...')
+
+
+
+
+
+
+
+
+# import logging
+#
+# # Если хочу дописывать, нужно поставить 'a'.
+# logging.basicConfig(level=logging.DEBUG,
+#                     filename='log_json_py.log',
+#                     filemode='w',
+#                     format='%(asctime)s %(levelname)s %(message)s')
+#
+# logging.debug('A DEBUG Message')
+# logging.info('AN INFO')
+# logging.warning('A WARNING')
+# logging.error('AN ERROR')
+# logging.critical('A message of CRITICAL severity')
+#
+# x_vals = [3, 5, 9, 7]
+# y_vals = [3, 8, 0, 2]
+#
+# for x_val, y_val in zip (x_vals, y_vals):
+#     x, y = x_val, y_val
+#     logging.info(f'The values of {x} and {y}.')
+#     try:
+#         a = x/y
+#         logging.info(f'x/y successful with result: {a}.')
+#     except ZeroDivisionError as err:
+#        # logging.error("ZeroDIVISIONERROR", exc_info=True)
+#         logging.exception('ZeroDivisionError')
+#
+#
+
+
+
+
+
+
 
 
 
@@ -90,7 +154,7 @@ if __name__ == '__main__':
 #         print(self.name, 'говорит: Мяу!')
 #
 #
-# my_cat = Cat('Барсик').meow()
+# my_cat = Cat('Барсик')
 #
 # my_cat.meow()
 #
